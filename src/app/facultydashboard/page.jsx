77719a703b4de9d693a2262a2d488a9bec076df2
KALYANI <22b01a12a0@svecw.edu.in>
2024-08-11 10:26:29 +0530

    sheet component for guest request form added

---
+++ src/app/facultydashboard/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+import GuestRequestForm from '../form/GuestRequestForm'; 
+
+const FacultyDashboard = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4 pt-20">Home</h1>
+      <GuestRequestForm />
+    </div>
+  );
+};
+
+export default FacultyDashboard;
